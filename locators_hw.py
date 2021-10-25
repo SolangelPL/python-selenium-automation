b29f8146621c998f8032d6d77b573326956a3874
Solangel <sp.lagares31@gmail.com>
2021-10-25 12:24:48 -0400

    Add files via upload

---
+++ locators_hw.py
@@ -0,0 +1,12 @@
+"""
+Logo, searched by, XPATH //*[@id="a-page"]/div[1]/div[1]
+Email field, searched by, ID "ap_email"
+Continue button, searched by, ID "continue"
+Need help link, searched by, Class "a-expander-prompt"
+Forgot your password link, searched by, ID "#auth-fpp-link-bottom"
+Other issues with sign-in link, searched by, ID "#ap-other-signin-issues-link"
+Create your Amazon account button, searched by, ID "#createAccountSubmit"
+Conditions of use link, searched by, XPATH "//*[@id="legalTextRow"]/a[1]"
+Privacy Notice link, searched by, XPATH "//*[@id="legalTextRow"]/a[2]"
+
+"""
